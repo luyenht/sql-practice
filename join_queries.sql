db6c953b5eeab79417c342c2bf691d96ff1316b3
Hoang Luyen <hoangluyen24@gmail.com>
2026-05-12 11:47:25 +0700

    update sql

--- join_queries.sql
+++ join_queries.sql
@@ -5,3 +5,12 @@ SELECT
 FROM Customers c
 INNER JOIN Orders o
     ON c.CustomerID = o.CustomerID;
+
+SELECT 
+    c.CustomerName,
+    o.OrderDate,
+    o.Amount
+FROM Customers c
+INNER JOIN Orders o
+    ON c.CustomerID = o.CustomerID;	
+	
