884e18bdae0eedd5cf98a65c57fa332489e2c2af
Hoang Luyen <hoangluyen24@gmail.com>
2026-05-12 15:19:28 +0700

    version 11

--- basic_queries.sql
+++ basic_queries.sql
@@ -42,22 +42,17 @@ ORDER BY city;
 SELECT name, city
 FROM Customers
 ORDER BY city;
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 7d23a55b2293bc2a703221e4f2c9a2511cedec95
 -- Lấy top 10
 SELECT top (10) *
 FROM Customers
 ORDER BY city;
-<<<<<<< HEAD
 
 -- Lấy tìm city
 SELECT * 
 FROM Customers
 where city='ninh bình';
-=======
+
 --- Tìm khách hàng có tên Luyến
 
 SELECT  *
@@ -65,4 +60,3 @@ FROM Customers
 ORDER BY city
  where name='luyen';
 
->>>>>>> 7d23a55b2293bc2a703221e4f2c9a2511cedec95
