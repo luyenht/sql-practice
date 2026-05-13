854d8d0681c46d33b9f85cfa1da386095193a33e
Hoang Luyen <hoangluyen24@gmail.com>
2026-05-13 09:25:20 +0700

    update code 13_05 lần 1

--- group_by.sql
+++ group_by.sql
@@ -1,5 +1,14 @@
+--- version 1---
+
 SELECT 
     CustomerID,
     SUM(Amount) AS TotalAmount
 FROM Orders
 GROUP BY CustomerID;
+
+--- version 2 group by theo name---
+SELECT 
+    Name,
+    SUM(Amount) AS TotalAmount
+FROM Orders
+GROUP BY Name;
